3b9bba14e7ae498d59928e2cc28f1e522239cf09
yanevannn <yanevan03@gmail.com>
2025-01-26 22:52:12 +0800

    update: implement PG-SQL

--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/notessServices');
 const init = async () => {
-    const server = Hapi.server({
-        port: 5001,
-        host: 'localhost',
-        // Menerapkan CORS pada Web Server
-        routes : {
-            cors : {
-                origin : ['*']
-            }
-        }
-    });
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT ,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
